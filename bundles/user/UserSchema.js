1ddb9e5169a68ebc0cedd879d65d01939dbed728
Marcelo M. Fleury <marcelomf@gmail.com>
2013-10-12 16:29:56 -0300

    Add facebook into UserSchema

--- bundles/user/UserSchema.js
+++ bundles/user/UserSchema.js
@@ -2,33 +2,41 @@ var UserSchema = function(di) {
 	validate = di.validate;
 	validator = di.validators.user;
 
+	this.mongooseFacebook = new di.mongoose.Schema({
+		like : Boolean,
+		share : Boolean,
+		friends_connected : [Integer],
+		linkesOnShare : Integer,
+		enableStream : Boolean
+	});
+	
 	this.json = {
 		id : di.mongoose.Schema.ObjectId,
-		username : {
-			type : String,
-			lowercase : true,
+		facebook: this.mongooseFacebook,
+		facebook_id : {
+			type : Integer,
 			required : true,
 			index : true,
-			unique : true,
-			trim : true,
-			validate : validator.username
+			unique : true
+		},
+		name : {
+			type : String,
+			required : true,
+			trim : true
 		},
 		email : {
 			type : String,
 			lowercase : true,
-			required : true,
+			required : false,
 			index : true,
 			unique : true,
 			trim : true,
 			validate : validate('isEmail')
 		},
-		password : {
-			type : String,
-			required : true,
-			validate : validator.password
-		}
+		age_range : String,
+		sex : String,
+		address : String
 	};
-
 	this.mongoose = new di.mongoose.Schema(this.json);
 };
 
