7a74ae5183e017701b832686311cba233feed866
Marcelo M. Fleury <marcelomf@gmail.com>
2013-10-13 15:18:29 -0300

    FooooooiiiiiiiiiiiiXXXXXXXXX ops

--- bundles/frontend/public/js/config.js
+++ bundles/frontend/public/js/config.js
@@ -1,4 +1,5 @@
 GraoJS = angular.module('GraoJS', ['ngResource', 'ui.bootstrap']);
 GraoJS.constant("config", {
-		baseUrl: "http://localhost:8015\:8015"
+		//baseUrl: "http://localhost:8015\:8015"
+		baseUrl: "http://192.168.1.215:8015"
 });
